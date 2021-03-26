ca2ea19376849fe3119798879e56a6d6defc2821
Victor Hugo <victorhugofp11@hotmail.com>
2021-03-26 09:59:10 -0700

    Acomodado para que funcione en c++,falta cambiar parametros y sintaxis

--- prueba.cpp
+++ prueba.cpp
@@ -1,31 +1,39 @@
+#include<iostream>
 #include <stdio.h>
 #include <math.h>
+using namespace std;
 #define e 2.718281828
 #define error 0.0001
 int main (){  
     int i=0;  
     float ea=100,x=0,xi=0,temp;  
     printf("Metodo de iteracion de punto fijo"); 
+    cout<<"\n";
     printf("Encontrar la raiz para la funcion:  f(x) = e^(-x) - x");  
+    cout<<"\n";
     printf("Con un error de 0.0001");
+    cout<<"\n";
      
-    printf("#       X             |Ea| \n\n\n");
+    cout<<"#       X             |Ea| \n\n\n";
     do {      
                   
             printf("%d",i);      
-                 
-            printf("%.8f",x);      
+            cout<<"\t";     
+            printf("%.8f",x);
+            cout<<"\t";      
                   
             xi = pow(e,(-x));      
             if (i==0)printf("------");      
             else{
-                    ea = fabs(((x-temp)/x)*100);printf("%.8f",ea);      
+                    ea = fabs(((x-temp)/x)*100);printf("%.8f",ea);
+                    cout<<"\t";      
                     }      
             temp=x;      
             x=xi;      
             i++;      
             printf("\n");    }
             while(ea > error);
+            cout<<"\n";
          
             printf("La raiz de la funcion es: %.8f",temp);  
               
